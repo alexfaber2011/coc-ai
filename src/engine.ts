9de0318aba537fcbc778d387565db42b70883dac
Kuro Latency <oozorakuro409@gmail.com>
2025-03-04 19:17:04 +0800

    refactor: too long for generate generator

--- src/engine.ts
+++ src/engine.ts
@@ -52,7 +52,7 @@ export class Engine {
     return { apiKey, orgId }
   }
 
-  async * generate(requestConfig: IEngineConfig, data: IAPIOptions) {
+  async #makeRequest(requestConfig: IEngineConfig, data: IAPIOptions) {
     this.config = requestConfig;
     const headers = {
       'Content-Type': 'application/json',
@@ -61,7 +61,7 @@ export class Engine {
         ...(this.token.orgId && { 'OpenAI-Organization': this.token.orgId })
       })
     };
-    const body = JSON.stringify(data)
+    const body = JSON.stringify(data);
 
     if (!this.controller.signal.aborted) this.controller.abort();
     this.controller = new AbortController();
@@ -69,66 +69,67 @@ export class Engine {
       this.controller.abort()
     }, this.config.requestTimeout * 1000);
 
-    try {
-      const resp = await fetch(this.config.endpointUrl, {
-        method: 'post',
-        body,
-        headers,
-        signal: this.controller.signal,
-      });
+    const resp = await fetch(this.config.endpointUrl, {
+      method: 'post',
+      body,
+      headers,
+      signal: this.controller.signal,
+    });
+    clearTimeout(timeout);
+    if (resp.status !== 200) {
+      window.showErrorMessage(`HTTPError ${resp.status}: ${resp.statusText}`);
+    }
+    return resp
+  }
 
-      if (!resp.ok || resp.body === null) {
-        window.showErrorMessage(`HTTPError ${resp.status}: ${resp.statusText}`);
-        return;
-      }
-      clearTimeout(timeout);
-      const reader = resp.body.getReader();
-      const decoder = new TextDecoder('utf-8');
-      let buffer = '';
+  #parseLine(line: string) {
+    const parsed = JSON.parse(line);
+    if (parsed.choices?.[0]?.delta?.reasoning_content) {
+      let chunk: IChunk = {
+        type: 'reasoning_content',
+        content: parsed.choices[0].delta.reasoning_content as string,
+      };
+      return chunk;
+    } else {
+      let chunk: IChunk = {
+        type: 'content',
+        content: parsed.choices[0].delta.content as string,
+      };
+      return chunk;
+    }
+  }
 
-      while (true) {
-        const { done, value } = await reader.read();
-        if (done || this.controller.signal.aborted) break;
+  async * generate(requestConfig: IEngineConfig, data: IAPIOptions) {
+    const resp = await this.#makeRequest(requestConfig, data)
+    if (!resp.body) return;
 
-        const data = decoder.decode(value, {stream: true});
-        const lines = data.split('\n').filter(line => line.trim() !== '');
+    const reader = resp.body.getReader();
+    const decoder = new TextDecoder('utf-8');
+    let buffer = '';
 
-        for (let line of lines) {
-          line = line.startsWith('data: ') ? line.slice('data: '.length) : line;
-          if (line === '[DONE]') continue;
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done || this.controller.signal.aborted) break;
+      const data = decoder.decode(value, {stream: true});
+      const lines = data.split('\n').filter(line => line.trim() !== '');
 
-          try {
-            if (buffer) {
-              line = buffer + line;
-              buffer = '';
-            }
-            const parsed = JSON.parse(line);
-            if (parsed.choices?.[0]?.delta?.reasoning_content) {
-              let chunk: IChunk = {
-                type: 'reasoning_content',
-                content: parsed.choices[0].delta.reasoning_content as string,
-              };
-              yield chunk;
-            } else if (parsed.choices?.[0]?.delta?.content) {
-              let chunk: IChunk = {
-                type: 'content',
-                content: parsed.choices[0].delta.content as string,
-              };
-              yield chunk;
-            }
-          } catch (error) {
-            if (buffer) {
-              window.showErrorMessage(`Error during decoding:${error}`);
-            } else {
-              buffer += line;  // in case json row scattered across two chunks
-            }
+      for (let line of lines) {
+        line = line.startsWith('data: ') ? line.slice('data: '.length) : line;
+        if (line === '[DONE]') continue;
+        try {
+          if (buffer) {
+            line = buffer + line;
+            buffer = '';
+          }
+          yield this.#parseLine(line);
+        } catch (error) {
+          if (buffer) {
+            window.showErrorMessage(`Error during decoding:${error}`);
+          } else {
+            buffer += line;  // in case json row scattered across two chunks
           }
         }
       }
-    } catch (error) {
-      if (error instanceof Error && error.name === 'AbortError') {
-        window.showErrorMessage('Request aborted...')
-      } else { throw error };
-    };
+    }
   }
 }
